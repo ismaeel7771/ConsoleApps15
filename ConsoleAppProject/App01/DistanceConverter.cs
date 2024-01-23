8b88745039d34594b92ed069c2dec0d945579f90
Ismaeel Omer <100208332@student.burnley.ac.uk>
2024-01-23 20:46:23 +0000

    Added empty methods and attributes

--- ConsoleAppProject/App01/DistanceConverter.cs
+++ ConsoleAppProject/App01/DistanceConverter.cs
@@ -10,5 +10,46 @@ namespace ConsoleAppProject.App01
     /// </author>
     public class DistanceConverter
     {
+        private double miles;
+
+        private double feet;
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+
+
+        public void DistanceConverter.Run()
+        {
+
+        }
+
+        /// <summary>
+        /// Prompt the user to enter thew distance in miles
+        /// Input the miles as a double number
+        /// </summary>
+        private void InputMiles ()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        private void CalculateFeet()
+        {
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+
+        private void OutputFeet()
+        {
+
+        }
+
     }
 }
