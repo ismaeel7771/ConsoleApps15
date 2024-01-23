9f29a6fb5fa1cf76a49923f36427e2f771674a9c
Ismaeel Omer <100208332@student.burnley.ac.uk>
2024-01-23 16:02:30 +0000

    Added my name and date

--- ConsoleAppProject/Program.cs
+++ ConsoleAppProject/Program.cs
@@ -11,7 +11,7 @@ namespace ConsoleAppProject
     /// to start App01 to App05 for CO453 CW1
     /// 
     /// This Project has been modified by:
-    /// Derek Peacock 05/02/2022
+    /// Ismaeel Omer  25/01/2024
     /// </summary>
     public static class Program
     {
