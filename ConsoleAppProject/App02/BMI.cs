2128d8cdb64b6897f4e8e9a795ba77a9ac8f8893
Ismaeel Omer <100208332@student.burnley.ac.uk>
2024-02-02 10:18:38 +0000

    Added BMI calculator

--- ConsoleAppProject/App02/BMI.cs
+++ ConsoleAppProject/App02/BMI.cs
@@ -1,12 +1,22 @@
 ﻿namespace ConsoleAppProject.App02
 {
-    /// <summary>
-    /// Please describe the main features of this App
-    /// </summary>
-    /// <author>
-    /// Student Name version 0.1
-    /// </author>
-    public class BMI
+    public static void Main()
+    {   //Variables to be declaired
+        int weight;
+        int height;
+        int bmi;
+
+
+        Console.WriteLine("Please Enter your height in inches: "); //Asks user for their height in inches
+        height = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter your weight in pounds: "); //Asks user for their weight in pounds
+        weight = Convert.ToInt32(Console.ReadLine());
+
+
+        bmi = (weight * 703) / (height * height); //Actual BMI calculation
+
+        if (bmi >= 18 && bmi <= 30) //Tests the users input, tells the user what their BMI is 
     {
     }
 }
